51a9d8d7f9866f07a9787b96c4a3b7c286e89479
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-03-27 11:16:30 +0000

    Refactored private FilterByMinThresholds method into its own class

---
+++ Week1/IThresholdFilterer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week1
+{
+    public interface IThresholdFilterer<T>
+    {
+        List<AssociationRule<T>> FilterByMinThresholds(List<IFact<T>> targetFacts, Database<T> projectedDatabase, List<ItemSet<IFact<T>>> frequentPatterns, List<AssociationRule<T>> candidateRules, double relativeMinsup, double minconf);
+    }
+}
---
+++ Week1/ThresholdFilterer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    public class ThresholdFilterer<T> : IThresholdFilterer<T>
+    {
+        public List<AssociationRule<T>> FilterByMinThresholds(List<IFact<T>> targetFacts, Database<T> projectedDatabase, List<ItemSet<IFact<T>>> frequentPatterns, List<AssociationRule<T>> candidateRules, Double relativeMinsup, Double minconf)
+        {
+            candidateRules.ForEach(candidateRule =>
+            {
+                var leftSet = frequentPatterns.Find(x => x.Equals(candidateRule.Left));
+                ItemSet<IFact<T>> rightSet;
+                ItemSet<IFact<T>> unionSet;
+
+                if (targetFacts == null)
+                {
+                    rightSet = frequentPatterns.Find(x => x.Equals(candidateRule.Right));
+                    unionSet = frequentPatterns.Find(x => x.Equals(candidateRule.Union()));
+
+                    candidateRule.AbsoluteSupport = unionSet.AbsoluteSupport;
+                    candidateRule.RelativeSupport = unionSet.RelativeSupport;
+                    candidateRule.Left.RelativeSupport = leftSet.RelativeSupport;
+                    candidateRule.Right.RelativeSupport = rightSet.RelativeSupport;
+                }
+                else
+                {
+                    candidateRule.AbsoluteSupport = leftSet.AbsoluteSupport;
+                    candidateRule.RelativeSupport = leftSet.RelativeSupport;
+                    candidateRule.Left.RelativeSupport = projectedDatabase.CalculateSupport(candidateRule.Left);
+                    candidateRule.Right.RelativeSupport = projectedDatabase.CalculateSupport(candidateRule.Right);
+                }
+                candidateRule.Confidence = candidateRule.RelativeSupport / candidateRule.Left.RelativeSupport;
+                candidateRule.LiftCorrelation = candidateRule.Confidence / candidateRule.Right.RelativeSupport;
+
+            });
+
+            return candidateRules.Where(rule => rule.RelativeSupport >= relativeMinsup && rule.Confidence >= minconf).ToList(); 
+        }
+        
+    }
+}
