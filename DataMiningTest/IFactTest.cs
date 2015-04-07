816fdfd01c679388ddf91737be39ad40c62e4e61
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-07 10:10:59 +0100

    Updated IFact tests

--- DataMiningTest/IFactTest.cs
+++ DataMiningTest/IFactTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataMining.Mocks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,27 +8,27 @@ using Xunit;
 
 namespace DataMining
 {
-    //public class IFactTest
-    //{
-    //    [Fact]
-    //    public void When_CompareTo_is_called_with_two_facts_of_same_type_it_should_give_the_opposite_result_to_the_equivalent_call_on_the_other_fact()
-    //    {
-    //        //Given
-    //        var fact = new SimpleFact("Black", "tf235");
-    //        var otherFact = new SimpleFact("White", "tailuge");
+    public class IFactTest
+    {
+        [Fact]
+        public void When_CompareTo_is_called_with_two_facts_of_same_type_it_should_give_the_opposite_result_to_the_equivalent_call_on_the_other_fact()
+        {
+            //Given
+            var fact = new MockFact("tf235");
+            var otherFact = new MockFact("tailuge");
 
-    //        Assert.Equal(fact.CompareTo(otherFact), - otherFact.CompareTo(fact));
-    //    }
+            Assert.Equal(fact.CompareTo(otherFact), - otherFact.CompareTo(fact));
+        }
 
-    //    [Fact]
-    //    public void When_CompareTo_is_called_with_a_different_fact_it_should_give_the_opposite_result_to_the_equivalent_call_on_the_other_fact()
-    //    {
-    //        //Given
-    //        var fact = new SimpleFact("Black", "tf235");
-    //        var otherFact = new OpeningFact("King's Gambit");
+        [Fact]
+        public void When_CompareTo_is_called_with_a_different_fact_it_should_give_the_opposite_result_to_the_equivalent_call_on_the_other_fact()
+        {
+            //Given
+            var fact = new MockFact("tf235");
+            var otherFact = new OtherMockFact("King's Gambit");
 
-    //        Assert.Equal(fact.CompareTo(otherFact), - otherFact.CompareTo(fact));
-    //    }
+            Assert.Equal(fact.CompareTo(otherFact), - otherFact.CompareTo(fact));
+        }
         
-    //}
+    }
 }
---
+++ DataMiningTest/Mocks/OtherMockFact.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMining.Mocks
+{
+    public class OtherMockFact : IFact<string>, IEquatable<OtherMockFact>
+    {
+        public OtherMockFact(string value)
+        {
+            this.Value = value;
+        }
+
+        public override bool IsTrue(string transaction)
+        {
+            return transaction.Contains(Value);
+        }
+
+        public override bool Implies(IFact<string> that)
+        {
+            if (that == null)
+            {
+                return false;
+            }
+
+            OtherMockFact fact = that as OtherMockFact;
+            return Implies(fact);
+        }
+
+        public bool Implies(OtherMockFact that)
+        {
+            if (that == null)
+            {
+                return false;
+            }
+
+            return this.Value.Equals(that.Value);
+        }
+
+        public bool Equals(OtherMockFact that)
+        {
+            if (that == null)
+            {
+                return false;
+            }
+            return this.Value.Equals(that.Value);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            OtherMockFact fact = obj as OtherMockFact;
+            if (fact == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Equals(fact);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
