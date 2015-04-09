ed100851b64bd1d49d7154df3001b9d3a6ae118c
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-09 11:48:31 +0100

    Added chess data mining class

---
+++ Week1/ChessDataMiner.cs
@@ -0,0 +1,40 @@
+﻿using ChessDataMining.FactGenerators;
+using DataMining;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessDataMining
+{
+    public class ChessDataMiner
+    {
+        private IEnumerable<ChessGame> games;
+
+        public ChessDataMiner(IEnumerable<ChessGame> games)
+        {
+            this.games = games;
+        }
+
+        public IEnumerable<AssociationRule<ChessGame>> Mine(Double relativeMinsup, Double minconf, List<IFact<ChessGame>> projectionFacts = null, List<IFact<ChessGame>> targetFacts = null)
+        {
+            //Fact Generators 
+            var openingFactsGenerator = new OpeningsFactsGenerator();
+            var simpleFactsGenerator = new SimpleFactsGenerator();
+            var timeControlFactsGenerator = new TimeControlFactsGenerator(new TimeControlCategoriser());
+            var takesFirstFactGenerator = new TakesFirstFactGenerator();
+
+            var candidateGenerator = new SelfJoinAndPruneGenerator<ChessGame>();
+            var factGenerators = new List<IFactsGenerator<ChessGame>>() { simpleFactsGenerator, openingFactsGenerator, timeControlFactsGenerator, takesFirstFactGenerator };
+            var apriori = new Apriori<ChessGame>(candidateGenerator, factGenerators);
+            var filterer = new ThresholdFilterer<ChessGame>();
+            var candidateRuleGenerator = new CandidateRuleGenerator<ChessGame>();
+
+            var database = new Database<ChessGame>(games.ToList());
+            var ruleGenerator = new AssociationRuleGenerator<ChessGame>(database, apriori, candidateRuleGenerator, filterer);
+
+            return ruleGenerator.Generate(relativeMinsup, minconf, projectionFacts, targetFacts);
+        }
+    }
+}
