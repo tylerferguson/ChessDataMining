8a9ff950839dd3e266d8c8ac8ae4357268684d8d
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-09 11:49:29 +0100

    Added post route Mine to server and javascript request

--- ChessMiningApp/Controllers/AssociationRulesController.cs
+++ ChessMiningApp/Controllers/AssociationRulesController.cs
@@ -13,66 +13,46 @@ using System.Web.Http;
 
 namespace ChessMiningApp.Controllers
 {
+    [RoutePrefix("api/AssociationRules")]
     public class AssociationRulesController : ApiController
     {
-        //refactor into separate class
-        private AssociationRuleGenerator<ChessGame> ruleGenerator;
-        private List<IFact<ChessGame>> projectedFacts;
-        private List<IFact<ChessGame>> targetFacts;
+        public AssociationRulesController() {}
 
-        public AssociationRulesController()
+        //POST api/AssociationRules/Mine
+        [Route("Mine")]
+        public IEnumerable<AssociationRuleDTO> Mine(List<ChessGame> games)
         {
-            //Refactor into separate class
-            List<ChessGame> result;
-            var serializer = new JsonSerializer();
-            using (var re = File.OpenText("C:/tferguson/Documents/Visual Studio 2013/Projects/PatternDiscoveryInDataMining/Week1/Week1/rated.json"))
-            using (var reader = new JsonTextReader(re))
-            {
-                result = serializer.Deserialize<List<ChessGame>>(reader);
-            }
-
-            var database = new Database<ChessGame>(result);
-
             var projectedFact1 = new OpeningFact("Gambit");
             //var projectedFact2 = new SimpleFact("White", "tailuge");
-            projectedFacts = new List<IFact<ChessGame>>() { projectedFact1 };
+            var projectedFacts = new List<IFact<ChessGame>>() { projectedFact1 };
 
             var targetFact = new SimpleFact("Result", "1-0");
-            targetFacts = new List<IFact<ChessGame>>() { targetFact };
+            var targetFacts = new List<IFact<ChessGame>>() { targetFact };
 
-            //Fact Generators 
-            var openingFactsGenerator = new OpeningsFactsGenerator();
-            var simpleFactsGenerator = new SimpleFactsGenerator();
-            var timeControlFactsGenerator = new TimeControlFactsGenerator(new TimeControlCategoriser());
-            var takesFirstFactGenerator = new TakesFirstFactGenerator();
-
-            var candidateGenerator = new SelfJoinAndPruneGenerator<ChessGame>();
-            var factGenerators = new List<IFactsGenerator<ChessGame>>() { simpleFactsGenerator, openingFactsGenerator, timeControlFactsGenerator, takesFirstFactGenerator };
-            var apriori = new Apriori<ChessGame>(candidateGenerator, factGenerators);
-            var filterer = new ThresholdFilterer<ChessGame>();
-            var candidateRuleGenerator = new CandidateRuleGenerator<ChessGame>();
-            ruleGenerator = new AssociationRuleGenerator<ChessGame>(database, apriori, candidateRuleGenerator, filterer);
-        }
-
-        //GET api/AssociationRules
-        public IEnumerable<AssociationRuleDTO> GetAssociationRules()
-        {
-            //Refactor into separate class
             var minsup = 0.01;
             var minconf = 0.1;
-            var rules = ruleGenerator.Generate(minsup, minconf, projectedFacts, targetFacts);
-            return rules.Select(x => 
+
+            var chessDataMiner = new ChessDataMiner(games);
+            var rules = chessDataMiner.Mine(minsup, minconf, projectedFacts, targetFacts);
+
+            return rules.Select(x =>
             {
-                return new AssociationRuleDTO() 
-                { 
-                    Value = x.ToString(), 
-                    AbsoluteSupport = x.AbsoluteSupport, 
-                    Confidence = x.Confidence, 
-                    LiftCorrelation = x.LiftCorrelation, 
-                    RelativeSupport = x.RelativeSupport 
+                return new AssociationRuleDTO()
+                {
+                    Value = x.ToString(),
+                    AbsoluteSupport = x.AbsoluteSupport,
+                    Confidence = x.Confidence,
+                    LiftCorrelation = x.LiftCorrelation,
+                    RelativeSupport = x.RelativeSupport
                 };
             });
         }
 
+        // POST api/AssociationRules/Mine
+        //[Route("Mine")]
+        //public string Mine(Byte[] games)
+        //{
+        //    return String.Empty;
+        //}
     }
 }
--- ChessMiningApp/app/mining.controller.js
+++ ChessMiningApp/app/mining.controller.js
@@ -2,9 +2,29 @@
 
     angular.module('ChessMining').controller('MiningCtrl', ['$scope', '$http', function ($scope, $http) {
 
+        var self = this;
+
+        $scope.submitDataFile = function () {
+            var file = $("#fileInput")[0].files[0];
+            console.log(file);
+            var reader = new FileReader();
+            reader.onloadend = function (event) {
+                console.log(event.target);
+               self.dataFile = event.target.result;
+            }
+
+            reader.onabort = function (event) {
+                console.log("i am aborted!");
+            }
+
+            reader.readAsText(file);
+        }
+
         $scope.getAssociationRules = function () {
-            $http.get('/api/AssociationRules')
+            console.log($.parseJSON(self.dataFile));
+            $http.post('/api/AssociationRules/Mine', JSON.parse(self.dataFile), { 'Content-Type': 'application / json' })
                 .then(function (response) {
+                    console.log(self.dataFile);
                     $scope.rules = [];
                     response.data.forEach(function (rule) {
                         $scope.rules.push(rule);
