7354983d3efe8a39e02b15327e4bedcb8af8d9d1
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-23 15:59:54 +0100

    Created basic button directive and template

---
+++ ChessMiningApp/app/addFilterActionButton/addFilterActionButton.directive.js
@@ -0,0 +1,6 @@
+﻿angular.module('ChessMining').directive("addFilterActionButton", function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/addFilterActionButton/add-filter-action-button-partial.html'
+    }
+})
