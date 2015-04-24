52b51a2208c1fa84b7629db372912c7caab6cda3
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-24 10:56:16 +0100

    Refactored more controller code into addFilterActionButton

--- ChessMiningApp/app/addFilterActionButton/addFilterActionButton.directive.js
+++ ChessMiningApp/app/addFilterActionButton/addFilterActionButton.directive.js
@@ -1,6 +1,75 @@
 ﻿angular.module('ChessMining').directive("addFilterActionButton", function () {
     return {
         restrict: 'E',
-        templateUrl: 'app/addFilterActionButton/add-filter-action-button-partial.html'
+        templateUrl: 'app/addFilterActionButton/add-filter-action-button-partial.html',
+        scope: {
+            facts: '=filters',
+            updateFilters: '&'
+        },
+        controller: ['$scope', '$http', function ($scope, $http) {
+
+            $scope.fact = '';
+            $scope.name = '';
+            $scope.value = '';
+            $scope.actionButtonClicked;
+            $scope.factOptions = {
+                facts: []
+                //key value pairs of individual facts
+            };
+
+            $scope.showActionMenu = function () {
+                $scope.actionButtonClicked = true;
+            }
+
+            $scope.closeActionMenu = function () {
+                setTimeout(function () {
+                    var focus = $(document.activeElement);
+                    if (focus.is("#fab") || $('#fab').has(focus).length) {
+                        console.log("still focused");
+                    } else {
+                        $scope.$apply(function () {
+                            $scope.actionButtonClicked = false;
+                        })
+                    }
+                }, 0);
+            }
+
+            $scope.submitFact = function () {
+                var fact = {
+                    type: $scope.fact,
+                    name: $scope.name,
+                    value: $scope.value
+                };
+
+                $scope.facts.push(fact);
+                $scope.fact = '';
+                $scope.name = '';
+                $scope.value = '';
+                $scope.updateFilters();
+            }
+
+            function getFactOptions() {
+                
+                $http.get('/api/AssociationRules', { 'Content-Type': 'application / json' })
+                    .then(function (response) {
+                        response.data.forEach(function (elem) {
+                            $scope.factOptions.facts.push(elem.FactType);
+                            $scope.factOptions[elem.FactType] = {};
+                            if (elem.FactType === "SimpleFact") {
+                                $scope.factOptions.SimpleFact.validNameParams = elem.ValidParams;
+                            } else {
+                                $scope.factOptions[elem.FactType].validValueParams = elem.ValidParams;
+                            }
+                        })
+                    },
+                    function (error) {
+                        console.log("get error!");
+                    });
+            }
+
+            getFactOptions();
+          
+
+        }]
     }
 })
--- ChessMiningApp/app/filter.controller.js
+++ ChessMiningApp/app/filter.controller.js
@@ -3,50 +3,7 @@
     var self = this;
     var factStage = $location.path().replace("/", "") + 's';
 
-    $scope.fact = '';
-    $scope.name = '';
-    $scope.value = '';
     $scope.facts = appService['get' + factStage]() || [];
-    $scope.actionButtonClicked;
-    $scope.factOptions = {
-        facts: []
-        //key value pairs of individual facts
-    };
-
-    $(document).ready(function () {
-        $http.get('/api/AssociationRules', { 'Content-Type': 'application / json' })
-            .then(function (response) {
-                response.data.forEach(function (elem) {
-                    $scope.factOptions.facts.push(elem.FactType);
-                    $scope.factOptions[elem.FactType] = {};
-                    if (elem.FactType === "SimpleFact") {
-                        $scope.factOptions.SimpleFact.validNameParams = elem.ValidParams;
-                    } else {
-                        $scope.factOptions[elem.FactType].validValueParams = elem.ValidParams;
-                    }
-                })
-            },
-            function (error) {
-                console.log("get error!");
-            });
-    });
-
-    $scope.showActionMenu = function () {
-        $scope.actionButtonClicked = true;
-    }
-
-    $scope.closeActionMenu = function () {
-        setTimeout(function () {
-            var focus = $(document.activeElement);
-            if (focus.is("#fab") || $('#fab').has(focus).length) {
-                console.log("still focused");
-            } else {
-                $scope.$apply(function () {
-                    $scope.actionButtonClicked = false;
-                })
-            }
-        }, 0);
-    }
 
     $scope.stopPropagation = function (event) {
         event.stopPropagation();
@@ -56,17 +13,7 @@
         event.preventDefault();
     }
 
-    $scope.submitFact = function () {
-        var fact = {
-            type: $scope.fact,
-            name: $scope.name,
-            value: $scope.value
-        };
-
-        $scope.facts.push(fact);
+    $scope.updateFilters = function () {
         appService['update' + factStage]($scope.facts);
-        $scope.fact = '';
-        $scope.name = '';
-        $scope.value = '';
     }
 }]);
