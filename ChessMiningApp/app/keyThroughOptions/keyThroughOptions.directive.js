12a8dd3cc8b82867c6c79fb22a0d391258cdb28a
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-24 16:29:30 +0100

    Added selectin of suggestions through arrow and enter keys

---
+++ ChessMiningApp/app/keyThroughOptions/keyThroughOptions.directive.js
@@ -0,0 +1,23 @@
+﻿angular.module('ChessMining').directive('keyThroughOptions', function () {
+    return {
+        restrict: 'A',
+        link: function ($scope, $elem, $attrs) {
+            
+            var current = -1;
+            $elem.on('keydown', function (event) {
+                if (event.which === 40) {
+                    current++;
+                } else if (event.which === 38) {
+                    current--;
+                } else if (event.which === 13) {
+                    if ($scope.label !== 'Value') {
+                        $scope.$apply($scope.selectFromSuggestedList($('.suggestion-focus')[0].innerText));
+                        event.preventDefault();
+                    } 
+                }
+                $('.suggestion-focus').removeClass('suggestion-focus');
+                $('.suggestion').eq(current).addClass('suggestion-focus');
+            })
+        }
+    }
+})
