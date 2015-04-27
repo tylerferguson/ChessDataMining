ab70049c0c0d412737cf2111ef8e8b65dd26b27e
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-27 11:41:11 +0100

    Cleaned up keythroughOptions directive code and fixed issue with empty submissions

--- ChessMiningApp/app/keyThroughOptions/keyThroughOptions.directive.js
+++ ChessMiningApp/app/keyThroughOptions/keyThroughOptions.directive.js
@@ -4,24 +4,33 @@
         link: function ($scope, $elem, $attrs) {
             
             var current = -1;
+            var currentSuggestion;
             $elem.on('keydown', function (event) {
                 if (event.which === 40 && current < $scope.filtered.length -1) {
-                    current++;
                     $('.suggestion-focus').removeClass('suggestion-focus');
-                    $($elem.next()[0].children[current]).addClass('suggestion-focus');
+                    currentSuggestion = nextSuggestion();
+                    currentSuggestion.addClass('suggestion-focus');
                 } else if (event.which === 38 && current > 0) {
-                    current--;
                     $('.suggestion-focus').removeClass('suggestion-focus');
-                    $($elem.next()[0].children[current]).addClass('suggestion-focus');
+                    currentSuggestion = previousSuggestion();
+                    currentSuggestion.addClass('suggestion-focus');
                 } else if (event.which === 13) {
                     if ($scope.label !== 'Value') {
-                        $scope.$apply($scope.selectFromSuggestedList($scope.filtered[current]));
-                        $scope.showOptions = false;
                         event.preventDefault();
+                    }
+                    if (current !== -1) {
+                        $scope.$apply($scope.selectFromSuggestedList($scope.filtered[current]));
                     }
                     current = -1;
                     $('.suggestion-focus').removeClass('suggestion-focus');
 
+                    function nextSuggestion() {
+                        return $($elem.next()[0].children[++current]);
+                    }
+
+                    function previousSuggestion() {
+                        return $($elem.next()[0].children[--current]);
+                    }
                 }
             })
         }
