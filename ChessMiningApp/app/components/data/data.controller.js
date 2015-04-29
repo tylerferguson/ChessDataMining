9f7636e3ce5fe73c4de657598ce4956dc02468ae
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-29 16:37:51 +0100

    Added data floating action button and data displayed in view window of data stage

--- ChessMiningApp/app/components/data/data.controller.js
+++ ChessMiningApp/app/components/data/data.controller.js
@@ -1,26 +1,9 @@
 ﻿angular.module('ChessMining').controller('DataCtrl', ['$scope', 'appService', function ($scope, appService) {
 
-    $scope.submitDataFile = function (event) {
-        var file = $("#fileInput")[0].files[0];
-        var reader = new FileReader();
+    $scope.dataFile = {};
+    $scope.dataFile.data = appService.getData() || [];
 
-        reader.onload = function (event) {
-            $scope.$apply(function () {
-                $scope.dataFile = event.target.result;
-                appService.updateData($scope.dataFile);
-            });
-        }
-
-        reader.onerror = function () {
-            $scope.$apply(function () {
-                $scope.dataFile = undefined;
-            });
-        }
-
-        if (file) {
-            reader.readAsText(file);
-        } else {
-            $scope.dataFile = undefined;
-        }
-    }
+    $scope.$watch('dataFile.data', function (newValue, oldValue) {
+        $scope.dataFile.displayData = JSON.stringify($scope.dataFile.data, null, 2);
+    });
 }]);
---
+++ ChessMiningApp/app/shared/addData/addData.directive.js
@@ -0,0 +1,32 @@
+﻿angular.module('ChessMining').directive('addData', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/shared/addData/add-data.html',
+        controller: ['$scope', 'appService', function ($scope, $appService) {
+
+            $scope.submitDataFile = function (event) {
+                var file = $("#fileInput")[0].files[0];
+                var reader = new FileReader();
+
+                reader.onload = function (event) {
+                    $scope.$apply(function () {
+                        $scope.dataFile.data = JSON.parse(event.target.result);
+                        $appService.updateData($scope.dataFile);
+                    });
+                }
+
+                reader.onerror = function () {
+                    $scope.$apply(function () {
+                        $scope.dataFile = undefined;
+                    });
+                }
+
+                if (file) {
+                    reader.readAsText(file);
+                } else {
+                    $scope.dataFile = undefined;
+                }
+            }
+        }]
+    }
+})
