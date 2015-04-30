c2d164833512a998fbcfc1400812d2a8aa7c41d2
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-04-30 11:29:29 +0100

    Change color scheme and add mine floating action button

--- ChessMiningApp/app/components/mine/mine.controller.js
+++ ChessMiningApp/app/components/mine/mine.controller.js
@@ -1,27 +1,4 @@
 ﻿angular.module('ChessMining').controller('MineCtrl', ['$scope', 'appService', '$http', function ($scope, appService, $http) {
 
     $scope.rules = appService.getResults() || [];
-
-    $scope.getAssociationRules = function () {
-
-        var dataTransferObject = {
-            games: appService.getData(),
-            minsup: $scope.minsup,
-            minconf: $scope.minconf,
-            projectionFacts: appService.getFilters(),
-            targetFacts: appService.getTargets()
-        };
-
-        $http.post('/api/AssociationRules/Mine', dataTransferObject, { 'Content-Type': 'application / json' })
-            .then(function (response) {
-                $scope.rules = [];
-                response.data.forEach(function (rule) {
-                    $scope.rules.push(rule);
-                })
-                appService.updateResults($scope.rules);
-            },
-            function (error) {
-                console.log('error!');
-            });
-    }
 }]);
---
+++ ChessMiningApp/app/shared/addMining/addMining.directive.js
@@ -0,0 +1,31 @@
+﻿angular.module('ChessMining').directive('addMining', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/shared/addMining/add-mining.html',
+        controller: ['$scope', '$http', 'appService', function ($scope, $http, $appService) {
+
+            $scope.getAssociationRules = function () {
+
+                var dataTransferObject = {
+                    games: $appService.getData(),
+                    minsup: $scope.minsup,
+                    minconf: $scope.minconf,
+                    projectionFacts: $appService.getFilters(),
+                    targetFacts: $appService.getTargets()
+                };
+
+                $http.post('/api/AssociationRules/Mine', dataTransferObject, { 'Content-Type': 'application / json' })
+                    .then(function (response) {
+                        $scope.rules.length = 0;
+                        response.data.forEach(function (rule) {
+                            $scope.rules.push(rule);
+                        })
+                        $appService.updateResults($scope.rules);
+                    },
+                    function (error) {
+                        console.log('error!');
+                    });
+            }
+        }]
+    }
+})
