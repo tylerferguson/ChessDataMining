545984198aa26f3ebbed643974d1d3468796e5e2
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-05-06 14:43:34 +0100

    Added dropbox button to add-data directive

--- ChessMiningApp/app/shared/addData/addData.directive.js
+++ ChessMiningApp/app/shared/addData/addData.directive.js
@@ -2,7 +2,7 @@
     return {
         restrict: 'E',
         templateUrl: 'app/shared/addData/add-data.html',
-        controller: ['$scope', '$timeout', 'appService', function ($scope, $timeout, $appService) {
+        controller: ['$scope', '$timeout', '$http', 'appService', function ($scope, $timeout, $http, $appService) {
             var dataFile = {};
             $scope.submitDataFile = function (event) {
                 event.preventDefault();
@@ -34,6 +34,25 @@
                 $timeout(function () {
                     $('#file-input').trigger('click');
                 }, 0, false)
+            }
+
+            $scope.triggerDropboxInput = function () {
+                var options = {
+                    success: function (files) {
+                        var file = files[0];
+                        $scope.$apply(function () {
+                            dataFile.name = file.name;
+                            $http.get(file.link).then(function (data) {
+                                dataFile.data = data;
+                                $appService.updateDataFile(dataFile);
+                            })
+                        });
+                    },
+                    linkType: 'direct',
+                    extensions: ['.json']
+                }
+
+                Dropbox.choose(options);
             }
         }]
     }
