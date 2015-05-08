84422968da13d3bb8af92334b71995719898b938
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-05-08 16:46:56 +0100

    Added button class to add filter button and fixed bug where only first filter was ever removed

--- ChessMiningApp/app/shared/filterPane/filterPane.directive.js
+++ ChessMiningApp/app/shared/filterPane/filterPane.directive.js
@@ -3,6 +3,7 @@
         restrict: 'E',
         templateUrl: 'app/shared/filterPane/filter-pane.html',
         scope: {
+            index: '@',
             fact: '=fact',
             facts: '=',
             factStage: '@'
@@ -10,8 +11,8 @@
         controller: ['$scope', 'appService', function ($scope, $appService) {
 
 
-            $scope.removeFact = function (index) {
-                $scope.facts.splice(index, 1);
+            $scope.removeFact = function () {
+                $scope.facts.splice($scope.index, 1);
                 $appService['update' + $scope.factStage]($scope.facts);
             }
         }]
