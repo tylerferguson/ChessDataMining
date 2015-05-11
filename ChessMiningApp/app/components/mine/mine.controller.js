99ddb2d72cb640e8283ce4c631db7e930103589d
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-05-11 11:18:06 +0100

    Added sorting to results table

--- ChessMiningApp/app/components/mine/mine.controller.js
+++ ChessMiningApp/app/components/mine/mine.controller.js
@@ -1,4 +1,12 @@
 ﻿angular.module('ChessMining').controller('MineCtrl', ['$scope', 'appService', '$http', function ($scope, appService, $http) {
 
     $scope.$appService = appService;
+
+    $scope.predicate = 'LiftCorrelation';
+    $scope.reverse = false;
+
+    $scope.setPredicateAndReverse = function (predicate) {
+        $scope.predicate = predicate;
+        $scope.reverse = !$scope.reverse;
+    }
 }]);
