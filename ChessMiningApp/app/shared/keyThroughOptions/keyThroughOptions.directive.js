57139f0a952e7ebc65341e896ab5de9a96e3d5ee
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-06-08 16:46:18 +0100

    Highlight now working on all options with up and down key arrows

--- ChessMiningApp/app/shared/keyThroughOptions/keyThroughOptions.directive.js
+++ ChessMiningApp/app/shared/keyThroughOptions/keyThroughOptions.directive.js
@@ -3,23 +3,33 @@
         restrict: 'A',
         link: function ($scope, $elem, $attrs) {
 
-            //console.log($('.' + $scope.factStage + '-filter-display' + ' .' + $scope.label + '-suggestion-field-input'));
+            $scope.$watch('filtered', function () {
+                if ($elem[0].innerText === $scope.filtered[0].replace('Fact', '')) {
+                    $elem.addClass('suggestion-focus');
+                }
+                $scope.current = 0;
+            }, true)
 
             $('.' + $scope.factStage + '-filter-display' + ' .' + $scope.label + '-suggestion-field-input').on('keydown', function (event) {
-                //console.log($elem[0].innerText);
-                //console.log($scope.filtered);
                 if (event.which === 40) {
                     console.log($scope.current);
+                    $scope.current++;
+
                     $elem.removeClass('suggestion-focus');
-                    if ($elem[0].innerText === $scope.filtered[$scope.current++ % $scope.filtered.length].replace('Fact', '')) {
+                    if ($elem[0].innerText === $scope.filtered[$scope.current % $scope.filtered.length].replace('Fact', '')) {
+                        $elem.addClass('suggestion-focus');
+                    }
+                } else if (event.which === 38) {
+                    $scope.current--;
+                    if ($scope.current < 0) {
+                        $scope.current = $scope.filtered.length + $scope.current;
+                    }
+                    $elem.removeClass('suggestion-focus');
+                    if ($elem[0].innerText === $scope.filtered[$scope.current % $scope.filtered.length].replace('Fact', '')) {
                         $elem.addClass('suggestion-focus');
                     }
                 } 
-                //else if (event.which === 38 && current > 0) {
-                //    $('.suggestion-focus').removeClass('suggestion-focus');
-                //    currentSuggestion = previousSuggestion();
-                //    currentSuggestion.addClass('suggestion-focus');
-                //} else if (event.which === 13 || event.which === 9) {
+                //else if (event.which === 13 || event.which === 9) {
                 //    if ($scope.label !== 'Value') {
                 //        event.preventDefault();
                 //    }
@@ -36,10 +46,10 @@
                 //    function previousSuggestion() {
                 //        return $($elem.next()[0].children[--current]);
                 //    }
-                //} else {
-                //    $('.suggestion-focus').removeClass('suggestion-focus');
-                //    $($elem.next()[0].children[current]).addClass('suggestion-focus');
-                //}
+                //} 
+                else {
+                    $elem.removeClass('suggestion-focus');
+                }
             })
             
             //var current = 0;
