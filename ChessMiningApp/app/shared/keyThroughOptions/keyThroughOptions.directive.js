6d6bf44a969cfd76d0054323c3554b44ab296513
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-06-08 17:04:37 +0100

    removed old keyThroughOptions directive code

--- ChessMiningApp/app/shared/keyThroughOptions/keyThroughOptions.directive.js
+++ ChessMiningApp/app/shared/keyThroughOptions/keyThroughOptions.directive.js
@@ -41,40 +41,6 @@
                     $elem.removeClass('suggestion-focus');
                 }
             })
-            
-            //var current = 0;
-            //var currentSuggestion;
-            //$elem.on('keydown', function (event) {
-            //    if (event.which === 40 && current < $scope.filtered.length -1) {
-            //        $('.suggestion-focus').removeClass('suggestion-focus');
-            //        currentSuggestion = nextSuggestion();
-            //        currentSuggestion.addClass('suggestion-focus');
-            //    } else if (event.which === 38 && current > 0) {
-            //        $('.suggestion-focus').removeClass('suggestion-focus');
-            //        currentSuggestion = previousSuggestion();
-            //        currentSuggestion.addClass('suggestion-focus');
-            //    } else if (event.which === 13 || event.which === 9) {
-            //        if ($scope.label !== 'Value') {
-            //            event.preventDefault();
-            //        }
-            //        if ($scope.filtered.length) {
-            //            $scope.$apply($scope.selectFromSuggestedList($scope.filtered[current]));
-            //        }
-            //        current = 0;
-            //        $('.suggestion-focus').removeClass('suggestion-focus');
-
-            //        function nextSuggestion() {
-            //            return $($elem.next()[0].children[++current]);
-            //        }
-
-            //        function previousSuggestion() {
-            //            return $($elem.next()[0].children[--current]);
-            //        }
-            //    } else {
-            //        $('.suggestion-focus').removeClass('suggestion-focus');
-            //        $($elem.next()[0].children[current]).addClass('suggestion-focus');
-            //    }
-            //})
         }
     }
 })
