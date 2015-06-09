2c7bb698d7bc7169ef9dd85e51b11e9e011ba63e
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-06-09 09:36:39 +0100

    Refactored keyThroughOptions directive

--- ChessMiningApp/app/shared/keyThroughOptions/keyThroughOptions.directive.js
+++ ChessMiningApp/app/shared/keyThroughOptions/keyThroughOptions.directive.js
@@ -4,21 +4,18 @@
         link: function ($scope, $elem, $attrs) {
 
             $scope.$watch('filtered', function () {
-                //if ($elem[0].innerText === $scope.filtered[0].replace('Fact', '')) {
-                //    $elem.addClass('suggestion-focus');
-                //}
                 $scope.current = null;
             }, true)
 
             $('.' + $scope.factStage + '-filter-display' + ' .' + $scope.label + '-suggestion-field-input').on('keydown', function (event) {
+
+                $elem.removeClass('suggestion-focus');
                 if (event.which === 40) {
                     if ($scope.current === null) {
                         $scope.current = -1;
                     }
                     $scope.current++;
-
-                    $elem.removeClass('suggestion-focus');
-                    if ($elem[0].innerText === $scope.filtered[$scope.current % $scope.filtered.length].replace('Fact', '')) {
+                    if (isCurrentlySelected()) {
                         $elem.addClass('suggestion-focus');
                     }
                 } else if (event.which === 38) {
@@ -29,26 +26,27 @@
                     if ($scope.current < 0) {
                         $scope.current = $scope.filtered.length + $scope.current;
                     }
-                    $elem.removeClass('suggestion-focus');
-                    if ($elem[0].innerText === $scope.filtered[$scope.current % $scope.filtered.length].replace('Fact', '')) {
+
+                    if (isCurrentlySelected()) {
                         $elem.addClass('suggestion-focus');
                     }
                 } else if (event.which === 13 || event.which === 9) {
                     if ($scope.current !== null) {
                         event.preventDefault();
-                        if ($elem[0].innerText === $scope.filtered[$scope.current % $scope.filtered.length].replace('Fact', '')) {
+                        if (isCurrentlySelected()) {
                             if ($scope.filtered.length) {
                                 $scope.$apply($scope.selectFromSuggestedList($scope.filtered[$scope.current % $scope.filtered.length]));
                             }
                         }
                     }
-                    
-                    $elem.removeClass('suggestion-focus');
                 } else {
                     $scope.current = null;
-                    $elem.removeClass('suggestion-focus');
                 }
             })
+
+            function isCurrentlySelected() {
+                return $elem[0].innerText === $scope.filtered[$scope.current % $scope.filtered.length].replace('Fact', '');
+            }
         }
     }
 })
